4113752d333784483a9411f279ef9feab5c98955
Mehluli Hikwa <mehlulihikwa@gmail.com>
2026-01-10 01:38:27 +0200

    Update End messsage

--- js/driver-js-theme.js
+++ js/driver-js-theme.js
@@ -12,7 +12,7 @@ const driverObj = driver({
         { element: '.intro-continent', popover: { title: 'Game Play', description: 'Start by selecting the continent you wish to explore.' } },
         { element: '.intro-difficulty', popover: { title: 'Level Difficulty', description: 'Next, choose the level of difficulty you want to play.' } },
         { element: '.intro-region', popover: { title: 'Region Selection', description: 'Finally, select the specific region you want to play with.' } },
-        { popover: { title: 'Good Luck', description: 'Have fun playing the game!' } }
+        { popover: { title: 'Good Luck', description: 'Have fun playing the game and learning about our worlds diversity!' } }
     ]
 });
 
