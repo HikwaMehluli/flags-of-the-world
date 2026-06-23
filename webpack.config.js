7bedcce47284d3174a147b1774b36c074fea07bb
Mehluli Hikwa <mehlulihikwa@gmail.com>
2026-06-23 12:50:33 +0200

    Simplify over-engineered JS compile

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,18 @@
 const path = require('path');
 
-const commonOutputPath = path.resolve(__dirname, 'dist');
-
-const createConfig = (entryFile, outputFilename, name) => ({
-    mode: 'production',
-    entry: `./js/${entryFile}`,
-    output: {
-        filename: outputFilename,
-        path: commonOutputPath,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-    name,
-});
-
-module.exports = [
-    createConfig('_entry.js', 'app.js', 'app'),
-];
+module.exports = {
+	mode: 'production',
+	entry: './js/_entry.js',
+	output: {
+		filename: 'app.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
+};
